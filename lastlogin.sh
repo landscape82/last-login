037ad64f6c7568b4ab6d761d07502dd7de995538
Venood Katuwa <venood.khatuva12@gmail.com>
2016-08-20 17:59:31 +0530

    Update lastlogin.sh

--- lastlogin.sh
+++ lastlogin.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Author: Vinod.N K
+#Distro : Linux -Centos, Rhel, and any fedora
 #Check whether root user is running the script
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
