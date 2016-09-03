6fbce6e677593aaddd2676d31a0762e73f000ac6
Venood Katuwa <vinod.katuwa12@gmail.com>
2016-09-03 15:32:20 +0530

    Update lastlogin.sh

--- lastlogin.sh
+++ lastlogin.sh
@@ -6,40 +6,45 @@ if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-rpm_present="`rpm -qa | grep mailx`"
-  if [ "$rpm_present" == "1" ]; then
-       	echo -e "\n MAilx already Installed  .. "
+ if rpm -q mailx &> /dev/null; then
+        echo "MAilx is installed..."
+    else
+        echo "Installing mailx..." && sudo yum install mailx -y
+    fi
+if rpm -q sendmail &> /dev/null; then
+        echo "Sendmail is installed..."
   else
-       	sudo yum install mailx
+       echo "Installing sendmail"... && sudo yum install sendmail -y
   fi
-sendml_present="`rpm -qa | grep sendmail`"
-  if [ "$sendml_present" == "1" ]; then
-       	echo -e "\n Sendmail already Installed  .. "
+if rpm -q newt &> /dev/null; then
+        echo "Newt is installed..."
   else
-       	sudo yum install sendmail
+       echo "Installing Newt"... && sudo yum install newt -y
   fi
+echo "restarting Sendmail..."
 sudo /etc/init.d/sendmail restart
-read -p "whats the hostname? : " host
+host=$(whiptail --title " LASTLOGIN SCRIPT " --inputbox "Please Enter hostname in CAPS? : " 10 60 HOSTNAME 3>&1 1>&2 2>&3)
 cd /usr/local/sbin/
-echo " #!/bin/bash
-RECIPIENT="devops@domainmoofwd.com";
-PREFIX="LOGIN ALERT OF $host!";
-REMOTEIP=$(/bin/echo $SSH_CLIENT | /usr/bin/awk '{ print $1 }');
-TIME=$(/bin/date +'%r, %D');
+echo '#!/bin/bash
+RECIPIENT="user@domain.com";
+PREFIX="LOGIN ALERT FROM '$host!'";
+REMOTEIP=$(/bin/echo $SSH_CLIENT | /usr/bin/awk "{ print $1 }");
+TIME=$(/bin/date +"%r, %D");
 HOST=$(/bin/hostname -f);
-
+ 
 if [[ "$REMOTEIP" == "" ]]; then
-   REMOTEIP='localhost';
+   REMOTEIP="localhost";
 fi
-
+ 
 /bin/cat <<LOGGEDIN |sudo /bin/mail -s "$PREFIX $USER@$HOST $TIME" $RECIPIENT
 Remote user $USER just logged in to $HOST at $TIME from $REMOTEIP
 LOGGEDIN
+ 
+exit $?
+' | sudo tee /usr/local/sbin/lastlogin
 
-exit $? " >> lastlogin
-
-chmod 755 lastlogin
+chmod 755 /usr/local/sbin/lastlogin
 
 echo "#last login configuration by Devops" >> /etc/profile
 echo  "/usr/local/sbin/lastlogin >/dev/null 2>&1 && disown -a" >> /etc/profile
-echo " LastLogin script has been added to the server Thanx for Using"
+echo "Thanx for Using..."
